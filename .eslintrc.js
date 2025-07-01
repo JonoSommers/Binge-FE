08ef801ae70683d0f970be1b221d4ec63b85404d
Jono Sommers <jonosommers95@gmail.com>
2025-07-01 12:41:20 -0500

    initial commit 5.0

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    // custom rules here if needed
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
